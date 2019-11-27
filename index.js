7fafb618364ced5045610770b73dbc3e9951e9dd
Philipp Renoth <renoth@aitch.de>
2019-11-27 15:19:57 +0100

    fix auth

--- index.js
+++ index.js
@@ -6,16 +6,24 @@ const toArrayBuffer = function() {
   let args = [...arguments];
 
   // Convert all arrays to buffers
-  args = args.flatMap(identity);
+  args = args.map(function(i) {
+    if (i instanceof Array) {
+      return Buffer.from(i);
+    }
+    return i;
+  })
+
+  // Merge into a single buffer
+  let buf = Buffer.concat(args);
 
   // Convert into ArrayBuffer
-  let ab = new ArrayBuffer(args.length);
+  let ab = new ArrayBuffer(buf.length);
   let view = new Uint8Array(ab);
-  for (let i = 0; i < args.length; ++i) {
-    view[i] = args[i];
+  for (let i = 0; i < buf.length; ++i) {
+    view[i] = buf[i];
   }
   return ab;
-};
+}
 
 const UUID_BASE = x => `0000${x}-0000-3512-2118-0009af100700`;
 
